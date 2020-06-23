858c617ffb8821794119ecb556eab8a27e41da84
Jack M <jackmccabe1995@hotmail.co.uk>
2020-06-23 23:05:31 +0100

    working prototype

---
+++ src/main/java/com/jackmccabe/spark/model/CourseIdea.java
@@ -0,0 +1,64 @@
+package com.jackmccabe.spark.model;
+
+import com.github.slugify.Slugify;
+
+import java.io.IOException;
+import java.util.*;
+
+public class CourseIdea {
+    private String slug;
+    private String title;
+    private String creator;
+    private Set<String> voters;
+
+    public CourseIdea(String title, String creator) {
+        voters = new HashSet<>();
+        this.title = title;
+        this.creator = creator;
+        Slugify slugify = new Slugify();
+        slug = slugify.slugify(title);
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public boolean addVoter(String voterUserName){
+        return  voters.add(voterUserName);
+    }
+
+    public List<String> getVoters(){
+        return new ArrayList<>(voters);
+    }
+
+    public int getVoteCount(){
+        return voters.size();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CourseIdea that = (CourseIdea) o;
+
+        if (!Objects.equals(title, that.title)) return false;
+        return Objects.equals(creator, that.creator);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = title != null ? title.hashCode() : 0;
+        result = 31 * result + (creator != null ? creator.hashCode() : 0);
+        return result;
+    }
+}
---
+++ src/main/java/com/jackmccabe/spark/model/NotFoundException.java
@@ -0,0 +1,4 @@
+package com.jackmccabe.spark.model;
+
+public class NotFoundException  extends  RuntimeException{
+}
